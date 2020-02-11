44de9a418dea5945e511d79ec8245c7a59ef3cbf
Preben <paa000@uit.no>
2020-02-11 15:30:36 +0100

    Not complete shit

--- chairs_per_person.py
+++ chairs_per_person.py
@@ -1,6 +1,12 @@
-f = open("reservations.csv")
+f = open("reservations")
 
 for reservation in f:
     name, number = reservation.split(",")
-    chairs_per_person = 50 / int(number)
+    try:
+        chairs_per_person = 50 / int(number)
+    except ValueError:
+        number = input("What is this number: {} --- in digits: ".format(number))
+        chairs_per_person = 50 / int(number)
+    except ZeroDivisionError or number == " ":
+        print("This \n\n\n")
     print("{} will get {} chairs per person".format(name, chairs_per_person))
